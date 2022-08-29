c7386a436af78da06cef4e6a271c5e0a754cd528
Arne <arne.carla.jacobs@outlook.com>
2022-08-29 18:40:28 +0200

    got velocity verlet working, the orbits are stable noe

--- src/physics.rs
+++ src/physics.rs
@@ -7,8 +7,9 @@ use ndarray::{Ix1, Array};
 use std::iter::zip;
 
 
-const GRAVITY_CONSTANT: f32 = 0.03;
-pub const DELTA_TIME: f64 = 0.01;
+const GRAVITY_CONSTANT: f32 = 0.09;
+// pub const DELTA_TIME: f64 = 0.0005;
+pub const DELTA_TIME: f64 = 0.001;
 
 
 #[derive(Component, Reflect)]
@@ -25,7 +26,6 @@ pub struct CelestialBody {
     pub mass: f32,
     pub vel: Vec3, 
     pub acc: Vec3, 
-    pub prev_acc: Vec3,
 }
 
 pub struct Consts {
@@ -38,10 +38,6 @@ pub struct CelestialBodyCalcBody {
     entity: Entity,
     celestial_body: CelestialBody,
     pos: Transform,
-}
-
-trait CalcBody<'a> {
-    fn fields(&'a mut self) -> (Entity, &'a mut Vec3, &'a mut CelestialBody);
 }
 
 impl From<&CelestialBodyCalcBody> for Consts {
@@ -64,22 +60,6 @@ impl From<CelestialBodyQueryItem<'_>> for CelestialBodyCalcBody {
    } 
 }
 
-impl<'a> CalcBody<'a> for CelestialBodyCalcBody {
-    fn fields(&'a mut self) -> (Entity, &'a mut Vec3, &'a mut CelestialBody) {
-        (self.entity, &mut self.pos.translation, &mut self.celestial_body)
-    }
-}
-
-impl<'a> CalcBody<'a> for CelestialBodyQueryItem<'_> {
-    fn fields(&'a mut self) -> (Entity, &'a mut Vec3, &'a mut CelestialBody) {
-        (self.entity, &mut self.pos.translation, &mut self.celestial_body)
-    }
-}
-
-trait StepCalcBody<'a> {
-    fn interact(&'a mut self, other: &'a mut Self);
-    fn integrate(&'a mut self, dt: f32, f: impl Integrator);
-}
 
 fn interact_gravity(
     pos1: &Vec3, _vel1: &Vec3, acc1: &mut Vec3, _consts1: &Consts,
@@ -102,7 +82,6 @@ pub struct CelestialBodyQuery {
     celestial_body: &'static mut CelestialBody,
     pos: &'static mut Transform, 
 }
-trait Integrator = Fn(f32, Vec3, Vec3, Vec3, Vec3) -> (Vec3, Vec3);
 
 pub fn step_system(
     mut query: Query<CelestialBodyQuery>,
@@ -206,7 +185,7 @@ impl System {
         };
 
         
-        let (new_pos, new_vel) = runge_kutta_4_nystorm(
+        let (new_pos, new_vel) = velocity_verlet(
             integrate_func, dt, time_passed, &self.pos_vec, &self.vel_vec
         );
         self.pos_vec = new_pos.clone();
@@ -215,14 +194,20 @@ impl System {
     }
 }
 
+trait Vectorspace<Num> = Sized
+where
+    for<'a> Num: Add<f32, Output=Num> + Mul<f32, Output=Num> + Add<Output=Num> + Add<&'a Num, Output=Num>, 
+    for<'b> &'b Num: Add<Num, Output=Num> + Add<&'b Num, Output=Num> + Mul<f32, Output=Num>;
+
+trait Integrator<Num> = for<'a> Fn(f32, &'a Num, &'a Num) -> Num;  
+
 // TODO make this interface a trait or type
 fn runge_kutta_4_nystorm<F, Num>(
     f: F, h: f32 /* dt */,  t0: f32, y0: &Num, dy0: &Num
 ) -> (Num, Num)
 where 
-    for<'a> Num: Add<Output=Num> + Add<&'a Num, Output=Num> + Add<f32, Output=Num> + Mul<f32, Output=Num>,
-    for<'c> &'c Num: Add<Num, Output=Num> + Add<&'c Num, Output=Num> + Mul<f32, Output=Num>,
-    F: for<'b> Fn(f32, &'b Num, &'b Num) -> Num, // f(t, x, x') = x'' or f(time, value, first-derivative) = second-derivative
+    Num: Vectorspace<Num>,
+    F: Integrator<Num>, // f(t, x', x) = x'' or f(time, first-derivative, value) = second-derivative
 {
     let k1 = f(t0, &dy0, &y0);
 
@@ -244,34 +229,51 @@ where
     (y, dy)
 }
 
-#[allow(dead_code)]
-fn runge_kutta_4<F, Num>(
-    f: F, 
-    h: f32, 
-    x0: Num, 
-    y0: Num
-) -> Num
-where
-    Num: Mul<f32, Output = Num> + Add<Num, Output=Num> + Add<f32, Output=Num> + Copy,
-    F: Fn(Num, Num) -> Num,
+// #[allow(dead_code)]
+// fn runge_kutta_4<F, Num>(
+//     f: F, 
+//     h: f32, 
+//     x0: Num, 
+//     y0: Num
+// ) -> Num
+// where
+//     Num: Mul<f32, Output = Num> + Add<Num, Output=Num> + Add<f32, Output=Num> + Copy,
+//     F: Fn(Num, Num) -> Num,
+// {
+//     let k1 = f(x0, y0);
+//     let k2 = f(x0 + h * 0.5, y0 + k1 * 0.5 * h);
+//     let k3 = f(x0 + 0.5 * h, y0 + k1 * 0.5 * h);
+//     let k4 = f(x0 + h, y0 + k3 * h);
+//     y0 + (k1 + k2 * 2.0 + k3 * 2.0 + k4) * (h / 6.0)
+// }
+
+fn velocity_verlet<F, Num>(
+    f: F, h: f32 /* dt */,  t0: f32, y0: &Num, dy0: &Num
+) -> (Num, Num)
+where 
+    Num: Vectorspace<Num>,
+    F: Integrator<Num>, // f(t, x', x) = x'' or f(time, first-derivative, value) = second-derivative
 {
-    let k1 = f(x0, y0);
-    let k2 = f(x0 + h * 0.5, y0 + k1 * 0.5 * h);
-    let k3 = f(x0 + 0.5 * h, y0 + k1 * 0.5 * h);
-    let k4 = f(x0 + h, y0 + k3 * h);
-    y0 + (k1 + k2 * 2.0 + k3 * 2.0 + k4) * (h / 6.0)
+    let ddy0 = f(t0, &dy0, y0);
+    let dy1_2 = dy0 + ddy0 * (h * 0.5); // v(t + 1/2 dt)
+    let y = y0 + &dy1_2 * h;
+    // let dy_approx = y 
+    let ddy = f(t0 + h, &dy1_2, &y); // probably an approximation of dy should be given here instead of dy1_2 
+    let dy = dy1_2 + ddy * (h * 0.5);
+
+    (y, dy)
 }
 
-#[allow(dead_code)]
-fn velocity_verlet(
-    dt: f32,
-    pos: Vec3,
-    vel: Vec3,
-    acc: Vec3,
-    prev_acc: Vec3,
-) -> (Vec3, Vec3) {
-    let dt_sq = dt*dt;
-    let new_pos = pos + vel * dt + acc * (dt_sq * 0.5);
-    let new_vel = vel + (acc + prev_acc) * (dt * 0.5);
-    return (new_pos, new_vel);
-}
+// #[allow(dead_code)]
+// fn velocity_verlet(
+//     dt: f32,
+//     pos: Vec3,
+//     vel: Vec3,
+//     acc: Vec3,
+//     prev_acc: Vec3,
+// ) -> (Vec3, Vec3) {
+//     let dt_sq = dt*dt;
+//     let new_pos = pos + vel * dt + acc * (dt_sq * 0.5);
+//     let new_vel = vel + (acc + prev_acc) * (dt * 0.5);
+//     return (new_pos, new_vel);
+// }
